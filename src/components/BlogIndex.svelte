054b9bf99d774d17f42b3c3cf22a81fbfbeda6bb
Hadrien Marquez <hadrienmarquez@gmail.com>
2021-10-11 17:10:06 +0200

    Blog index component for the post on the homepage

---
+++ src/components/BlogIndex.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { goto } from '$app/navigation';
+    export let posts;  
+
+</script>
+
+<main>
+    <ul>
+        {#each posts as {path, metadata: {title, date}}}
+            <li on:click={() => {goto(`/${path.replace(".md","")}`)}}>
+                    <h5>
+                        {title}
+                    </h5>
+                    <p class="small">{new Date(date).toLocaleDateString("fr-FR")}</p>
+            </li>
+            
+        {/each}
+    </ul>
+</main>
+
+
+<style>
+    main {
+        overflow-y: scroll;
+        border: 2px solid black ;
+        grid-column:  2/3;
+        grid-row: 2/3;
+        
+    
+    }
+
+    h5 {margin: 0px;
+        padding: 0.5rem 0 0;
+    }
+
+    ul {
+        background-color: lightgreen;
+        position: relative;
+        top: 0px;
+    }
+
+    li {
+        cursor: pointer;
+    }
+
+    li:nth-child(2n) {
+        background-color: lightgray;
+    }
+
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,82 @@
-<h1>Désert libre</h1>
+<script context="module">
+
+    // Posts metadata retrieving
+    const modules = import.meta.glob("./posts/*.md");
+    export const allPosts = [];
+
+    for(let path in modules) {
+        allPosts.push(modules[path]().then(({metadata}) => {
+            return {path, metadata};
+        }));
+    }
+
+    export const load = async () => {
+
+    const posts = await Promise.all(allPosts);
+
+
+    return {
+        props: {
+            posts,
+        },
+    };
+    };
+
+</script>
+
+<script lang="ts">
+        import BlogIndex from "/src/components/BlogIndex.svelte";
+        export let posts;
+
+        // Sorting posts by date.
+        // @ts-ignore
+        posts.sort((post1, post2) => {
+            const date1: Date =  new Date(post1.metadata.date);
+            const date2: Date =  new Date(post2.metadata.date);
+            return   date2.getTime() - date1.getTime();
+        })
+</script>
+
+<!-- Global things to add to head -->
+<svelte:head>
+    <title>Désert libre</title>
+</svelte:head>
+
+<div class="panel grid">
+    <!-- Title -->
+    <h1 class="title huge">Désert libre</h1>
+
+
+    <!-- Liste des blogs -->
+    <BlogIndex {posts}>
+
+    </BlogIndex>
+
+
+    <!-- Bas de page -->
+
+</div>
+
+<style>
+    .panel {
+        width: 70%;
+        height: 100vh;
+        margin: 0px 15vw;
+    }
+
+    .grid {
+        display: grid;
+        grid-template-columns: 1fr 3fr 1fr;
+        grid-template-rows: 3fr 4fr 2fr;
+    }
+
+    .title {
+        font-weight: bold;
+        grid-column:  2/3;
+        justify-self: center;
+        align-self: center;
+    }
+
+
+
+</style>
